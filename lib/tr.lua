5802540281af23e65c9fb6fb9c62cf65fcfe614a
Alexander Carusillo <alexcarusillo@gmail.com>
2024-07-27 18:25:11 -0400

    paramquencer added

--- lib/tr.lua
+++ lib/tr.lua
@@ -1,12 +1,229 @@
 local tr_lanes = 
     {
-        { clock_mod = 1, clock_idx = nil, pattern = {}, current_position = 1},
-        { clock_mod = 1, clock_idx = nil, pattern = {}, current_position = 1},
-        { clock_mod = 1, clock_idx = nil, pattern = {}, current_position = 1},
-        { clock_mod = 1, clock_idx = nil, pattern = {}, current_position = 1}
+        { clock_mod = 1, clock_idx = nil, pattern = {}, current_position = 1, paramquencer = {}},
+        { clock_mod = 1, clock_idx = nil, pattern = {}, current_position = 1, paramquencer = {}},
+        { clock_mod = 1, clock_idx = nil, pattern = {}, current_position = 1, paramquencer = {}},
+        { clock_mod = 1, clock_idx = nil, pattern = {}, current_position = 1, paramquencer = {}}
     }
 
 local tr_api = {}
+
+
+-- Paramquencer Config
+-- TODO: Break this out into own script?
+local paramquencer = {}
+
+function get_sequenced_param(param_idx, tr_idx)
+
+    -- local selected_param_index = params:get('sequenced_param_' .. arp_idx)
+  
+    local sequenceable_params = {
+        "strum_duration_" .. tr_idx,
+        "strum_pulse_count_" .. tr_idx,
+        "strum_clustering_percent_" .. tr_idx,
+        "strum_clustering_variation_" .. tr_idx,
+        "strum_rhythm_" .. tr_idx,
+        "burst_count_" .. tr_idx,
+        "burst_trigger_interval_" .. tr_idx,
+        "burst_randomization_amount_" .. tr_idx,
+        "burst_rhythm_" .. tr_idx
+    }
+  
+    return sequenceable_params[param_idx]
+  end
+
+function paramquencer.init(idx)
+    tr_lanes[idx].paramquencer = {
+        pulse = {0, 0, 0, 0},
+        step = {0, 0, 0, 0}
+    }
+end
+
+function paramquencer.add_params(idx)
+    paramquencer.init(idx)
+
+    local sequenceable_params = {
+        "strum_duration_" .. idx,
+        "strum_pulse_count_" .. idx,
+        "strum_clustering_percent_" .. idx,
+        "strum_clustering_variation_" .. idx,
+        "strum_rhythm_" .. idx,
+        "burst_count_" .. idx,
+        "burst_trigger_interval_" .. idx,
+        "burst_randomization_amount_" .. idx,
+        "burst_rhythm_" .. idx
+    }
+    
+    params:add_separator("paramquencer_separator_" .. idx, "Paramquencer")
+
+    params:add_number("paramquencer_lanes_tr_" .. idx, "Lane", 1, 4, 1)
+    params:set_action("paramquencer_lanes_tr_" .. idx, function(new_lane)
+        paramquencer.update_lane_visibility(idx, new_lane)
+    end)
+
+    for lane_idx = 1, 4 do
+        params:add_separator("paramquencer_lane_" .. idx .. "_" .. lane_idx, "Edit Lane " .. lane_idx)
+        local param_names = {
+            "S: Duration",
+            "S: Pulse Count",
+            "S: Cluster %",
+            "S: Cluster Variation",
+            "S: Rhythm",
+            "B: Count",
+            "B: Trigger Interval",
+            "B: Random Amount",
+            "B: Rhythm"
+        }
+        params:add_option("paramquencer_param_tr_" .. idx .. "_" .. lane_idx, "Parameter", param_names, 1)
+        params:set_action("paramquencer_param_tr_" .. idx .. "_" .. lane_idx, function(value)
+            local param_meta = params:lookup_param(sequenceable_params[value])
+            -- tab.print(param_meta)
+
+            if param_meta.options then
+              for step_idx = 1, 6 do
+                local this_step = params:lookup_param("paramquencer_step_tr_" .. idx .. "_" .. lane_idx .. "_" .. step_idx)
+                this_step.options = param_meta.options
+                this_step.count = #param_meta.options
+                this_step.selected = 1
+              end
+            elseif param_meta.controlspec then
+                -- tab.print(param_meta.controlspec)
+              for step_idx = 1, 6 do
+                local this_step = params:lookup_param("paramquencer_step_tr_" .. idx .. "_" .. lane_idx .. "_" .. step_idx)
+                local values = {}
+                for i = param_meta.controlspec.minval, param_meta.controlspec.maxval do
+                    table.insert(values, i)
+                end
+                this_step.options = values
+                this_step.count = #values
+                this_step.selected = 1
+              end
+            end
+            _menu.rebuild_params()
+        end)
+
+        params:add_number("paramquencer_steps_tr_" .. idx .. "_" .. lane_idx, "Steps", 0, 6, 0)
+        params:set_action("paramquencer_steps_tr_" .. idx .. "_" .. lane_idx, function(steps)
+            for step_idx = 1, 6 do
+                if step_idx <= steps then
+                    params:show("paramquencer_step_tr_" .. idx .. "_" .. lane_idx .. "_" .. step_idx)
+                else
+                    params:hide("paramquencer_step_tr_" .. idx .. "_" .. lane_idx .. "_" .. step_idx)
+                end
+            end
+            _menu.rebuild_params()
+        end)
+        params:add_number("paramquencer_pulses_tr_" .. idx .. "_" .. lane_idx, "Pulses per Step", 1, 64, 12)
+        
+        for step_idx = 1, 6 do
+            params:add_option("paramquencer_step_tr_" .. idx .. "_" .. lane_idx .. "_" .. step_idx, "Step " .. step_idx, {0, 1}, 1)
+        end
+    end
+
+    paramquencer.update_lane_visibility(idx, 1)
+end
+
+function paramquencer.update_lane_visibility(idx, active_lane)
+    for lane = 1, 4 do
+        if lane == active_lane then
+            params:show("paramquencer_lane_" .. idx .. "_" .. lane)
+            params:show("paramquencer_param_tr_" .. idx .. "_" .. lane)
+            params:show("paramquencer_steps_tr_" .. idx .. "_" .. lane)
+            params:show("paramquencer_pulses_tr_" .. idx .. "_" .. lane)
+            local steps = params:get("paramquencer_steps_tr_" .. idx .. "_" .. lane)
+            for step = 1, 6 do
+                if step <= steps then
+                    params:show("paramquencer_step_tr_" .. idx .. "_" .. lane .. "_" .. step)
+                else
+                    params:hide("paramquencer_step_tr_" .. idx .. "_" .. lane .. "_" .. step)
+                end
+            end
+        else
+            params:hide("paramquencer_lane_" .. idx .. "_" .. lane)
+            params:hide("paramquencer_param_tr_" .. idx .. "_" .. lane)
+            params:hide("paramquencer_steps_tr_" .. idx .. "_" .. lane)
+            params:hide("paramquencer_pulses_tr_" .. idx .. "_" .. lane)
+            for step = 1, 6 do
+                params:hide("paramquencer_step_tr_" .. idx .. "_" .. lane .. "_" .. step)
+            end
+        end
+    end
+    _menu.rebuild_params()
+end
+
+function paramquencer.process(idx)
+    for lane = 1, 4 do  -- Always process all 4 lanes
+        local steps = params:get("paramquencer_steps_tr_" .. idx .. "_" .. lane)
+        
+        if steps > 0 then  -- Only process if there are steps
+            local pulses = params:get("paramquencer_pulses_tr_" .. idx .. "_" .. lane)
+            
+            tr_lanes[idx].paramquencer.pulse[lane] = tr_lanes[idx].paramquencer.pulse[lane] + 1
+            if tr_lanes[idx].paramquencer.pulse[lane] > pulses then
+                tr_lanes[idx].paramquencer.pulse[lane] = 1
+                tr_lanes[idx].paramquencer.step[lane] = (tr_lanes[idx].paramquencer.step[lane] % steps) + 1
+                
+                local next_value = params:get("paramquencer_step_tr_" .. idx .. "_" .. lane .. "_" .. tr_lanes[idx].paramquencer.step[lane])
+                local param_idx = params:get("paramquencer_param_tr_" .. idx .. "_" .. lane)
+                local param_id = get_sequenced_param(param_idx, idx)
+
+                -- Adjust the next_value based on the parameter type
+                local param_meta = params:lookup_param(param_id)
+                if param_meta.controlspec then
+                    -- For number parameters, subtract 1 from the value
+                    next_value = next_value - 1
+                end
+
+                -- print("Updating param:", param_id, "to value:", next_value)
+                params:set(param_id, next_value)
+
+                _menu.rebuild_params()
+            end
+        end
+    end
+end
+
+function paramquencer.reset()
+    for lane = 1, 4 do
+        tr_lanes[idx].paramquencer.pulse[lane] = 0
+        tr_lanes[idx].paramquencer.step[lane] = 0
+    end
+end
+
+local function start_clock(idx)
+    local this_lane = tr_lanes[idx]
+
+    while true do
+        paramquencer.process(idx)  -- Process paramquencer for this TR lane
+
+        local probability = params:get("play_probability_tr_" .. idx)
+
+        if math.random() * 100 <= probability then
+            if params:get("mode_tr_" .. idx) == 1 then  -- Clock 
+                crow.ii.txo.tr_pulse(idx)
+            elseif params:get("mode_tr_" .. idx) == 2 then -- Strum
+                initiate_strum(idx)
+            elseif params:get("mode_tr_" .. idx) == 3 then -- Burst
+                trigger_burst(idx)
+            elseif params:get("mode_tr_" .. idx) == 4 then -- Euclidean 
+                next_euclidean_step(idx)
+            end
+        end
+
+        clock.sync(this_lane.clock_mod)
+    end
+end
+
+local function toggle_play_state(idx)
+    local this_lane = tr_lanes[idx]
+
+    if this_lane.clock_idx == nil then
+        this_lane.clock_idx = clock.run(start_clock, idx)
+    else
+        clock.cancel(this_lane.clock_idx)
+        this_lane.clock_idx = nil
+    end
+end
 
 -- Adapted from https://github.com/monome/bowery/blob/main/euclidean.lua & https://gist.github.com/vrld/b1e6f4cce7a8d15e00e4
 local function generate_euclidean_pattern(idx)
@@ -71,39 +288,6 @@ local function generate_euclidean_pattern(idx)
     end   
 
     tr_lanes[idx].pattern = final_pattern
-end
-
-local function start_clock(idx)
-    local this_lane = tr_lanes[idx]
-
-    while true do
-        local probability = params:get("play_probability_tr_" .. idx)
-
-        if math.random() * 100 <= probability then
-            if params:get("mode_tr_" .. idx) == 1 then  -- Clock 
-                crow.ii.txo.tr_pulse(idx)
-            elseif params:get("mode_tr_" .. idx) == 2 then -- Strum
-                initiate_strum(idx)
-            elseif params:get("mode_tr_" .. idx) == 3 then -- Burst
-                trigger_burst(idx)
-            elseif params:get("mode_tr_" .. idx) == 4 then -- Euclidean 
-                next_euclidean_step(idx)
-            end
-        end
-
-        clock.sync(this_lane.clock_mod)
-    end
-end
-
-local function toggle_play_state(idx)
-    local this_lane = tr_lanes[idx]
-
-    if this_lane.clock_idx == nil then
-        this_lane.clock_idx = clock.run(start_clock, idx)
-    else
-        clock.cancel(this_lane.clock_idx)
-        this_lane.clock_idx = nil
-    end
 end
 
 -- Shared Setup
@@ -186,7 +370,7 @@ function add_strum_params(idx)
     params:add{type = "control", id = "strum_clustering_percent_" .. idx, name = "Clustering", 
         controlspec = controlspec.new(0, 100, 'lin', 1, 50, "%")}
     params:add{type = "control", id = "strum_clustering_variation_" .. idx, name = "Variation", 
-        controlspec = controlspec.new(0, 1, 'lin', 0.01, 0, "")}
+        controlspec = controlspec.new(0, 100, 'lin', 1, 0, "%")}
     params:add_option("strum_rhythm_" .. idx, "Rhythm", {"Even", "Dotted", "Triplet", "Swing"}, 1)
 end
 
@@ -214,7 +398,7 @@ function generate_strum_timing(idx)
     local total_duration = clock.get_beat_sec() * duration_beats
     local num_pulses = params:get("strum_pulse_count_" .. idx)
     local clustering = params:get("strum_clustering_percent_" .. idx) / 100
-    local variation = params:get("strum_clustering_variation_" .. idx)
+    local variation = params:get("strum_clustering_variation_" .. idx) / 100  -- Convert to 0-1 range
 
     local pulse_times = {}
     local min_time = 0.001 -- Minimum time in seconds (1ms)
@@ -289,9 +473,9 @@ function add_burst_params(idx)
     params:add_separator("burst_mode_header_" .. idx, "Burst")
     params:add{type = "control", id = "burst_count_" .. idx, name = "Trigger Count", 
         controlspec = controlspec.new(0, 20, 'lin', 1, 3, "triggers")}
-        params:add_option("trigger_interval_" .. idx, "Trigger Interval", 
+        params:add_option("burst_trigger_interval_" .. idx, "Trigger Interval", 
         {"1/32", "1/16", "1/8", "1/4", "1/2", "1", "2", "4"}, 3)
-    params:add{type = "control", id = "randomization_amount_" .. idx, name = "Humanize Amount", 
+    params:add{type = "control", id = "burst_randomization_amount_" .. idx, name = "Humanize Amount", 
         controlspec = controlspec.new(0, 10, 'lin', 0.1, 0, "%")}
     params:add_option("burst_rhythm_" .. idx, "Rhythm", {"Even", "Dotted", "Triplet", "Swing"}, 1)
 end
@@ -299,25 +483,25 @@ end
 function show_burst_params(idx)
     params:show("burst_mode_header_" .. idx)
     params:show("burst_count_" .. idx)
-    params:show("trigger_interval_" .. idx)
-    params:show("randomization_amount_" .. idx)
+    params:show("burst_trigger_interval_" .. idx)
+    params:show("burst_randomization_amount_" .. idx)
     params:show("burst_rhythm_" .. idx)
 end
 
 function hide_burst_params(idx)
     params:hide("burst_mode_header_" .. idx)
     params:hide("burst_count_" .. idx)
-    params:hide("trigger_interval_" .. idx)
-    params:hide("randomization_amount_" .. idx)
+    params:hide("burst_trigger_interval_" .. idx)
+    params:hide("burst_randomization_amount_" .. idx)
     params:hide("burst_rhythm_" .. idx)
 end
 
 function trigger_burst(idx)
     local interval_options = {1/32, 1/16, 1/8, 1/4, 1/2, 1, 2, 4}
-    local interval_beats = interval_options[params:get("trigger_interval_" .. idx)]
+    local interval_beats = interval_options[params:get("burst_trigger_interval_" .. idx)]
     local trigger_interval = clock.get_beat_sec() * interval_beats
     local burst_count = params:get("burst_count_" .. idx)
-    local randomization_percentage = params:get("randomization_amount_" .. idx)
+    local randomization_percentage = params:get("burst_randomization_amount_" .. idx)
     local rhythm_type = params:get("burst_rhythm_" .. idx)
 
     local randomization_amount = trigger_interval * (randomization_percentage / 100)
@@ -396,12 +580,13 @@ function next_euclidean_step(idx)
 end
 
 function tr_api:add_txo_tr_params(idx)
-    params:add_group("telexo_tr_config" .. idx, "TR " .. idx, 22)
+    params:add_group("telexo_tr_config" .. idx, "TR " .. idx, 64)
     add_shared_params(idx)
     add_clock_params(idx)
     add_strum_params(idx)
     add_burst_params(idx)
     add_euclidean_params(idx)
+    paramquencer.add_params(idx)
     hide_strum_params(idx)
     hide_burst_params(idx)
     hide_euclidean_params(idx)
@@ -411,6 +596,7 @@ function tr_api:sync_triggers()
     for i = 1, 4 do
         tr_lanes[i].current_position = 1
     end
+    paramquencer.reset()
 end
 
 return tr_api
